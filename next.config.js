aecfec4ed87db211fa7df578a24a6c0f7b77f96e
Hari-1410 <hari14.off@gmail.com>
2026-03-11 22:55:59 +0530

    Fix next.config standalone output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
+  output: "standalone",
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = nextConfig;
